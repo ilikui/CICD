6c69c64dddad163ad652d68d57d337ed95beeec7
ilikui <2563043733@qq.com>
2023-06-03 07:48:08 +0800

    update

--- code/createm.m
+++ code/createm.m
@@ -4,4 +4,6 @@ disp("###############Start###############");
  h =new_system('Mini','model');
 
  save_system(h);
+ 
+Simulink.BlockDiagram.saveActiveConfigSet(h, 'mdlCfg.m')
 disp("###############End###############");
