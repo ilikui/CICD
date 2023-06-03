0aefb20222d9e52096f7e999e3321152c49dc7f4
ilikui <2563043733@qq.com>
2023-06-03 16:32:56 +0800

    update

--- Model/Script/Createm.m
+++ Model/Script/Createm.m
@@ -4,9 +4,12 @@ function Createm(mdName,filePath)
 
     currentPath=pwd;
     ShowLog(currentPath);
-    ValidFilePath =strcat(currentPath,'/','Model','/',filePath,'/',mdName);
+    ValidFilePath =strcat(currentPath,'/','Model','/',filePath,'/');
+    
+    cd(ValidFilePath)
     ShowLog(ValidFilePath)
     h =new_system(mdName,'model');
-    save_system(ValidFilePath);
+    save_system(h);
     Simulink.BlockDiagram.saveActiveConfigSet(h, strcat(currentPath,'/','Model','/',filePath,'/','mdlCfg.m'))  
+    
 end
