a2da10f2fbea2e26098f0af219d578b357b15bc2
ilikui <2563043733@qq.com>
2023-06-03 17:02:14 +0800

    update

--- Model/Script/Createm.m
+++ Model/Script/Createm.m
@@ -1,7 +1,13 @@
 
 function Createm(mdName)
     
-    ShowLog(ValidFilePath)
+
+
+    ShowLog(pwd)
+    if 4==exist(mdName,'file')
+        ShowLog("model is exist");
+        return
+    end
     h =new_system(mdName);
     save_system(h);
     Simulink.BlockDiagram.saveActiveConfigSet(h,'mdlCfg.m');
